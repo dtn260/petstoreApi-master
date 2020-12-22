a2ffef00d9c877659f829a59b7db5e5ca178b285
doruk.taskin <doruk.taskin@n11.com>
2020-12-22 15:20:15 +0300

    petshop commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+### Test rapport is te vinden:
+Open ./build/reports/tests/test/index.html bestand openen in de browser.
---
+++ src/test/java/helpers/RestAssuredHelper.java
@@ -0,0 +1,31 @@
+package helpers;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+
+public class RestAssuredHelper {
+    public RestAssuredHelper(){
+        String url = System.getProperty("server.url");
+        if(url==null) {
+            url="https://petstore.swagger.io";
+        }
+        RestAssured.baseURI = url;
+    }
+
+    public RequestSpecification requestSpec() {
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+        builder.addHeader("api_key", "special-key");
+        builder.addHeader("Content-Type", "application/json");
+        return builder.build();
+    }
+
+    public ResponseSpecification responseSpec() {
+        ResponseSpecBuilder builder = new ResponseSpecBuilder();
+        builder.expectStatusCode(200);
+        builder.expectContentType("application/json");
+        return builder.build();
+    }
+}
